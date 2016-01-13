e67288d369cc9c624b54c8bfdbf1c3d67f262997
dstarcev <starcev.da@gmail.com>
2016-01-14 02:54:24 +0300

    first try

---
+++ src/HexletBattleDotNet/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using Newtonsoft.Json;
+
+using NUnit.Framework;
+
+public class Program {
+	public static int Main(string[] args) {
+		var cases = GetStdinLines()
+			.Select(JsonConvert.DeserializeObject<ExpandoObject>);
+
+		foreach (IDictionary<string, object> testCase in cases) {
+			if (testCase.ContainsKey("check")) {
+				Console.WriteLine(testCase["check"]);
+				return 0;
+			}
+
+			try {
+				var actual = SolutionClass.Solution((IList<object>)testCase["arguments"]);
+				var actualJson = JsonConvert.SerializeObject(actual);
+				var expectedJson = JsonConvert.SerializeObject(testCase["expected"]);
+				Assert.AreEqual(expectedJson, actualJson);
+			}
+			catch (Exception ex) {
+				Console.WriteLine(ex);
+				break;
+			}
+		}
+
+		return 1;
+	}
+
+	private static IEnumerable<string> GetStdinLines() {
+		string line;
+		while (!string.IsNullOrEmpty(line = Console.ReadLine())) {
+			yield return line;
+		}
+	}
+}
---
+++ src/HexletBattleDotNet/Solution.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class SolutionClass {
+	// collections represented as IList<object>
+	// maps represented as IDictionary<string, object>
+	// integers represented as long
+	// floats represented as double
+	public static object Solution(IList<object> args) {
+		var keys = (ICollection<object>)args[0];
+		return keys.ToDictionary(k => k, v => args[1]);
+	}
+}
+
